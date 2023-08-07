1c3ab3ce222c671b32dde94a446eb670d009e003
tenith <tenith@hotmail.com>
2023-08-07 19:08:56 +0700

    hot fix, add menu Personal document(s) onto production and revise position of delete icon on ONE-SEP.component.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  appVersion: require('../../package.json').version + '-dev',
+  appVersion: require("../../package.json").version + "-dev",
   production: false,
   /*
     01 Mar 2023 wutthichair
       Add firebaseConfig
   */
-  firebaseConfig : {
-    
+  firebaseConfig: {
     apiKey: "AIzaSyAZXCFg_Kf4Gf9KsH7FhoFLIbXdCktjqSk",
     authDomain: "lightredcenter.firebaseapp.com",
     projectId: "lightredcenter",
@@ -33,35 +32,61 @@ export const environment = {
     // messagingSenderId: "880704583837",
     // appId: "1:880704583837:web:312aec2438ff06a7017c74",
     // measurementId: "G-NC095M4JX4",
-    vapidKey: "BOve8TKsfSKrRzdpsJy-cyKljdgWpbChZnnbCL_USVFzmvSwNMPUxchiH0_83O-jwNl5Z7XpX5g_V2t1XfKrdhE"
-  }
+    vapidKey:
+      "BOve8TKsfSKrRzdpsJy-cyKljdgWpbChZnnbCL_USVFzmvSwNMPUxchiH0_83O-jwNl5Z7XpX5g_V2t1XfKrdhE",
+  },
 };
 
 export const API = {
-  cloudFunctionAnnouncement: 'https://us-central1-lightredcenter.cloudfunctions.net/sendNotification',
-  cloudFunctionVRNotification: 'https://us-central1-lightredcenter.cloudfunctions.net/sendVRNotification',
-  cloudFunctionETS1Notification: 'https://us-central1-lightredcenter.cloudfunctions.net/sendETS1Notification',
-  cloudFunctionPersonalDocNotification: 'https://us-central1-lightredcenter.cloudfunctions.net/sendPersonalDocNotification',  
-  cloudFunctionCCDTEAMPersonalDocNotification: 'https://us-central1-red-center.cloudfunctions.net/sendCCDPersonalDocNotification',
-  
-  sepGoogleService: 'https://script.google.com/macros/s/AKfycbwmXXEWXuc9wgU-Wp579t5ssz3MIwZe7Zyz1qlOz-Ox99nIsrhWiHemkezY7T3ClY7N/exec',    
-  autolandGoogleSerivce: 'https://script.google.com/macros/s/AKfycbyVqLFc02e78IYpjEZYg6YW67fEYQIBiYi9FKXrh1ze3SB8oDz53lCLyxriLmIT72uJ/exec',
-  newsGoogleService: 'https://script.google.com/macros/s/AKfycby3jym8YGxvVAo4uOh6D5L1w_nwQzS7Mr_w3Tu-LS-xnkbJeUzvLPaz7Zbuo36mT2RL/exec',
-  performanceGoogleService: 'https://script.google.com/macros/s/AKfycbyAPnu61zvXx0AJ1xP0P8pNN1rkq7wqFi09x31W6vXtR2Ze66AKXgaRx7oXUrz8iz57/exec',
-  
-  vrCommentGoogleService: 'https://script.google.com/macros/s/AKfycbxl0ej86EJ8E61Btz-3a86yzrd01nhEksxtxCvubM5P99QM3VGlvM-1HWfctRD76CCl/exec',
-  vrGoogleService: 'https://script.google.com/macros/s/AKfycbzwDk-vZNTX4NGy7buN2wDFErz3MzBaFC8wniDIX9BVkOUVTeeXJXKhDTtpKXwJxGaz/exec',
-  eTS1GoogleService: 'https://script.google.com/macros/s/AKfycbxW1rmS__ee8VXQsn35v-1URReJYWLCVe_1bOeyoft0wHTrWnDReX-aDg1MiNkJAnWc/exec',    
+  cloudFunctionAnnouncement:
+    "https://us-central1-lightredcenter.cloudfunctions.net/sendNotification",
+  cloudFunctionVRNotification:
+    "https://us-central1-lightredcenter.cloudfunctions.net/sendVRNotification",
+  cloudFunctionETS1Notification:
+    "https://us-central1-lightredcenter.cloudfunctions.net/sendETS1Notification",
+  cloudFunctionPersonalDocNotification:
+    "https://us-central1-lightredcenter.cloudfunctions.net/sendPersonalDocNotification",
+  cloudFunctionCCDTEAMPersonalDocNotification:
+    "https://us-central1-red-center.cloudfunctions.net/sendCCDPersonalDocNotification",
+
+  sepGoogleService:
+    "https://script.google.com/macros/s/AKfycbwmXXEWXuc9wgU-Wp579t5ssz3MIwZe7Zyz1qlOz-Ox99nIsrhWiHemkezY7T3ClY7N/exec",
+  autolandGoogleSerivce:
+    "https://script.google.com/macros/s/AKfycbyVqLFc02e78IYpjEZYg6YW67fEYQIBiYi9FKXrh1ze3SB8oDz53lCLyxriLmIT72uJ/exec",
+  newsGoogleService:
+    "https://script.google.com/macros/s/AKfycby3jym8YGxvVAo4uOh6D5L1w_nwQzS7Mr_w3Tu-LS-xnkbJeUzvLPaz7Zbuo36mT2RL/exec",
+  performanceGoogleService:
+    "https://script.google.com/macros/s/AKfycbyAPnu61zvXx0AJ1xP0P8pNN1rkq7wqFi09x31W6vXtR2Ze66AKXgaRx7oXUrz8iz57/exec",
+
+  vrCommentGoogleService:
+    "https://script.google.com/macros/s/AKfycbxl0ej86EJ8E61Btz-3a86yzrd01nhEksxtxCvubM5P99QM3VGlvM-1HWfctRD76CCl/exec",
+  vrGoogleService:
+    "https://script.google.com/macros/s/AKfycbzwDk-vZNTX4NGy7buN2wDFErz3MzBaFC8wniDIX9BVkOUVTeeXJXKhDTtpKXwJxGaz/exec",
+  eTS1GoogleService:
+    "https://script.google.com/macros/s/AKfycbxW1rmS__ee8VXQsn35v-1URReJYWLCVe_1bOeyoft0wHTrWnDReX-aDg1MiNkJAnWc/exec",
 
   // redBookTMC: 'https://red-u.thaiairasia.co.th/training-api-prelaunch/v1/Redbook/GetRedbook',
   // redBookTMC_X: '7eOOFH0SIRlp9HLlVAMo28YYm56cRZNQ',
 
-  redBookTMC: 'https://red-u.thaiairasia.co.th/training-api/v1/Redbook/GetRedbook',
-  redBookTMC_X: 'cbHBWdeRODn2f6H1xca52FYSIRc4WfC9',
+  redBookTMC:
+    "https://red-u.thaiairasia.co.th/training-api/v1/Redbook/GetRedbook",
+  redBookTMC_X: "cbHBWdeRODn2f6H1xca52FYSIRc4WfC9",
 };
 
-export const baseURL = 'https://lightredcenter.web.app/';
+export const baseURL = "https://lightredcenter.web.app/";
 
-export const clickActionVR = 'https://lightredcenter.web.app/pages/eVR/workspace?id=';
+export const clickActionVR =
+  "https://lightredcenter.web.app/pages/eVR/workspace?id=";
 
-export const showMenu = ['Home', 'Authorization', 'Documents Amendment', 'Personal document(s)', 'Documents verification', 'E-VR', 'My performance', 'E-TS1', 'SEP Card', 'Log out'];
+export const showMenu = [
+  "Home",
+  "Authorization",
+  "Documents Amendment",
+  "Personal document(s)",
+  "Documents verification",
+  "E-VR",
+  "My performance",
+  "E-TS1",
+  "SEP Card",
+  "Log out",
+];
